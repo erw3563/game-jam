1d97468171f9ba4803bc5170b051873d15a428ec
erw3563 <3591481985@qq.com>
2025-11-23 10:35:04 +0800

    刺

---
+++ Entity/Thron/thron.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var throned_body_velocity_y:float = -128
+@export var throned_body_position_x:float = 16
+@export var is_fall:bool##刺是否会下落
+
+func _on_damage_component_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		body.velocity.y = throned_body_velocity_y
+		var body_x_dir = signf(body.velocity.x)
+		body.position.x -= body_x_dir * throned_body_position_x
+	if body is TileMapLayer:
+		queue_free()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		if is_fall:
+			var tween = create_tween()
+			tween.tween_property(self,"position",position + Vector2.DOWN * 128,1)
